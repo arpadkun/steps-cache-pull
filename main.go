aaf817e8ca7561966c3c5782d1d2e7182a414779
Arpad Kun <kharpeet@bela.lan>
2020-08-14 18:12:33 -0700

    test run

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,7 +9,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"path/filepath"
+	"os/exec"
 	"strings"
 	"time"
 
@@ -156,98 +157,194 @@ func main() {
 	stepconf.Print(conf)
 	log.SetEnableDebugLog(conf.DebugMode)
 
-	if conf.CacheAPIURL == "" {
-		log.Warnf("No Cache API URL specified, there's no cache to use, exiting.")
-		return
-	}
+	// if conf.CacheAPIURL == "" {
+	// 	log.Warnf("No Cache API URL specified, there's no cache to use, exiting.")
+	// 	return
+	// }
 
 	startTime := time.Now()
 
-	var cacheReader io.Reader
-	var cacheURI string
+	//var cacheReader io.Reader
+	//var cacheURI string
 
-	if strings.HasPrefix(conf.CacheAPIURL, "file://") {
-		cacheURI = conf.CacheAPIURL
+	// if strings.HasPrefix(conf.CacheAPIURL, "file://") {
+	// 	cacheURI = conf.CacheAPIURL
 
-		fmt.Println()
-		log.Infof("Using local cache archive")
+	// 	fmt.Println()
+	// 	log.Infof("Using local cache archive")
 
-		pth := strings.TrimPrefix(conf.CacheAPIURL, "file://")
+	// 	pth := strings.TrimPrefix(conf.CacheAPIURL, "file://")
 
-		var err error
-		cacheReader, err = os.Open(pth)
-		if err != nil {
-			failf("Failed to open cache archive file: %s", err)
-		}
-	} else {
-		fmt.Println()
-		log.Infof("Downloading remote cache archive")
+	// 	var err error
+	// 	cacheReader, err = os.Open(pth)
+	// 	if err != nil {
+	// 		failf("Failed to open cache archive file: %s", err)
+	// 	}
+	// } else {
+	// 	fmt.Println()
+	// 	log.Infof("Downloading remote cache archive")
 
-		downloadURL, err := getCacheDownloadURL(conf.CacheAPIURL)
-		if err != nil {
-			failf("Failed to get cache download url: %s", err)
-		}
-		cacheURI = downloadURL
+	// 	downloadURL, err := getCacheDownloadURL(conf.CacheAPIURL)
+	// 	if err != nil {
+	// 		failf("Failed to get cache download url: %s", err)
+	// 	}
+	// 	cacheURI = downloadURL
 
-		cacheReader, err = performRequest(downloadURL)
-		if err != nil {
-			failf("Failed to perform cache download request: %s", err)
-		}
-	}
+	// 	cacheReader, err = performRequest(downloadURL)
+	// 	if err != nil {
+	// 		failf("Failed to perform cache download request: %s", err)
+	// 	}
+	// }
 
-	cacheRecorderReader := NewRestoreReader(cacheReader)
+	// cacheRecorderReader := NewRestoreReader(cacheReader)
 
-	currentStackID := strings.TrimSpace(conf.StackID)
-	if len(currentStackID) > 0 {
-		fmt.Println()
-		log.Infof("Checking archive and current stacks")
-		log.Printf("current stack id: %s", currentStackID)
+	// currentStackID := strings.TrimSpace(conf.StackID)
+	// if len(currentStackID) > 0 {
+	// 	fmt.Println()
+	// 	log.Infof("Checking archive and current stacks")
+	// 	log.Printf("current stack id: %s", currentStackID)
 
-		r, hdr, err := readFirstEntry(cacheRecorderReader)
-		if err != nil {
-			failf("Failed to get first archive entry: %s", err)
-		}
+	// 	r, hdr, err := readFirstEntry(cacheRecorderReader)
+	// 	if err != nil {
+	// 		failf("Failed to get first archive entry: %s", err)
+	// 	}
 
-		cacheRecorderReader.Restore()
+	// 	cacheRecorderReader.Restore()
 
-		if filepath.Base(hdr.Name) == "archive_info.json" {
-			b, err := ioutil.ReadAll(r)
-			if err != nil {
-				failf("Failed to read first archive entry: %s", err)
-			}
+	// 	if filepath.Base(hdr.Name) == "archive_info.json" {
+	// 		b, err := ioutil.ReadAll(r)
+	// 		if err != nil {
+	// 			failf("Failed to read first archive entry: %s", err)
+	// 		}
 
-			archiveStackID, err := parseStackID(b)
-			if err != nil {
-				failf("Failed to parse first archive entry: %s", err)
-			}
-			log.Printf("archive stack id: %s", archiveStackID)
+	// 		archiveStackID, err := parseStackID(b)
+	// 		if err != nil {
+	// 			failf("Failed to parse first archive entry: %s", err)
+	// 		}
+	// 		log.Printf("archive stack id: %s", archiveStackID)
 
-			if archiveStackID != currentStackID {
-				log.Warnf("Cache was created on stack: %s, current stack: %s", archiveStackID, currentStackID)
-				log.Warnf("Skipping cache pull, because of the stack has changed")
-				os.Exit(0)
-			}
-		} else {
-			log.Warnf("cache archive does not contain stack information, skipping stack check")
-		}
-	}
+	// 		if archiveStackID != currentStackID {
+	// 			log.Warnf("Cache was created on stack: %s, current stack: %s", archiveStackID, currentStackID)
+	// 			log.Warnf("Skipping cache pull, because of the stack has changed")
+	// 			os.Exit(0)
+	// 		}
+	// 	} else {
+	// 		log.Warnf("cache archive does not contain stack information, skipping stack check")
+	// 	}
+	// }
 
 	fmt.Println()
 	log.Infof("Extracting cache archive")
 
-	if err := extractCacheArchive(cacheRecorderReader); err != nil {
-		log.Warnf("Failed to uncompress cache archive stream: %s", err)
-		log.Warnf("Downloading the archive file and trying to uncompress using tar tool")
+	// if err := extractCacheArchive(cacheRecorderReader); err != nil {
+	// 	log.Warnf("Failed to uncompress cache archive stream: %s", err)
+	// 	log.Warnf("Downloading the archive file and trying to uncompress using tar tool")
 
-		pth, err := downloadCacheArchive(cacheURI)
-		if err != nil {
-			failf("Fallback failed, unable to download cache archive: %s", err)
-		}
+	// 	pth, err := downloadCacheArchive(cacheURI)
+	// 	if err != nil {
+	// 		failf("Fallback failed, unable to download cache archive: %s", err)
+	// 	}
 
-		if err := uncompressArchive(pth); err != nil {
-			failf("Fallback failed, unable to uncompress cache archive file: %s", err)
-		}
+	// 	if err := uncompressArchive(pth); err != nil {
+	// 		failf("Fallback failed, unable to uncompress cache archive file: %s", err)
+	// 	}
+	// }
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	log.Printf("-----------------------------AKARMI Printf-----------------------------")
+	log.Infof("-----------------------------AKARMI Infof-----------------------------")
+	fmt.Println("-----------------------------AKARMI fmt.println-----------------------------")
+
+	// Requirements, set up ENV Variables as secrets <-----:
+	// LOCAL_CACHE_DST_URL
+	// LOCAL_CACHE_KEY
+
+	// Getting the ssh key into variable
+	LocalCacheKey := os.Getenv("LOCAL_CACHE_KEY")
+	LocalCacheKeyDecoded, _ := base64.URLEncoding.DecodeString(LocalCacheKey)
+
+	// Write the ssh key to file
+	HomeDir := os.Getenv("HOME")
+	LocalCacheStorageSSHKeyFile := HomeDir + "/.ssh/local_cache.key"
+	LocalCacheFilesListFile := HomeDir + "/.local_cache_file_list"
+	LocalCacheFilesDstURL := os.Getenv("LOCAL_CACHE_DST_URL")
+	LocalCacheStoragePort := "22"
+	LocalCacheStoragePortTimeout := "3"
+
+	// Write ssh key file
+	file, err := os.Create(LocalCacheStorageSSHKeyFile)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		file.WriteString(string(LocalCacheKeyDecoded))
 	}
+	file.Close()
+
+	err = os.Chmod(LocalCacheStorageSSHKeyFile, 0600)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//////////////////////////////////////////
+	// Configuring rsync parameters
+	rsyncSettingsSSHsetup := "/usr/bin/ssh -i " + LocalCacheStorageSSHKeyFile + " -o ConnectTimeout=" + LocalCacheStoragePortTimeout + " -p " + LocalCacheStoragePort
+	rsyncSettingsFilesFrom := "--files-from=" + LocalCacheFilesListFile
+	rsyncSettingsDestinationURL := LocalCacheFilesDstURL
+
+	// Downloading file list first
+	log.Infof("Downloading file list first...")
+	rsyncArgsListOnly := []string{"-e", rsyncSettingsSSHsetup, "--dirs", "--archive", "--no-D", "--inplace", "--executability", "--ignore-errors", "--force", "--compress", "--stats", "--human-readable", "--no-whole-file", "--prune-empty-dirs", rsyncSettingsDestinationURL + LocalCacheFilesListFile, HomeDir}
+	fmt.Printf("%v\n\n", rsyncArgsListOnly)
+
+	cmdGetList := exec.Command("rsync", rsyncArgsListOnly...)
+
+	outputGetList, err := cmdGetList.CombinedOutput()
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %v\n", err.Error()))
+	}
+	log.Printf("%v\n", string(outputGetList))
+
+	// Syncing down the files using the downloaded list
+	log.Infof("Syncing files now...")
+	rsyncArgs := []string{"-e", rsyncSettingsSSHsetup, rsyncSettingsFilesFrom, "--dirs", "--relative", "--archive", "--no-D", "--inplace", "--executability", "--delete", "--ignore-errors", "--force", "--compress", "--stats", "--human-readable", "--no-whole-file", "--prune-empty-dirs", rsyncSettingsDestinationURL, "/"}
+	fmt.Printf("%v\n\n", rsyncArgs)
+
+	cmd := exec.Command("rsync", rsyncArgs...)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %v\n", err.Error()))
+	}
+	log.Printf("%v\n", string(output))
+
+	// K:  cycling through the files/directories that are required to be saved
+	// log.Printf("\n============================================================================================")
+	// for pth := range pathToIndicatorPath {
+	// 	// log.Printf("%s", pth)
+	// 	// log.Printf("============================================================================================")
+	// 	// log.Printf("This is in the pathToIndicatorPath variable: %s", pth)
+	// 	var cmd1 = exec.Command("file", pth)
+	// 	output, err := cmd1.Output()
+	// 	if err != nil {
+	// 		log.Printf("Could not run find, failed")
+	// 	}
+	// 	log.Printf("%v\n", string(output))
+
+	// 	// // If the path is directory, let's print the contents
+	// 	// if info, err := os.Stat(pth); err == nil && info.IsDir() {
+	// 	// 	var cmd2 = exec.Command("find", pth)
+	// 	// 	output, err := cmd2.Output()
+	// 	// 	if err != nil {
+	// 	// 		log.Printf("Could not run find, failed")
+	// 	// 	}
+	// 	// 	log.Printf("------------ Directory [%s] Contents ------------:\n  %v\n", pth, string(output))
+	// 	// }
+	// }
+
+	log.Printf("============================================================================================")
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	fmt.Println()
 	log.Donef("Done")
